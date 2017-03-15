45e7e4430cd2ecd5b73254cc7e246c64aea41d46
Stephane Rolland <stephane.rolland.soat@gmail.com>
2017-03-15 17:59:56 +0100

    read master ip, and add some hspec tests

--- tzubox/src/UserConfig.hs
+++ tzubox/src/UserConfig.hs
@@ -7,30 +7,49 @@ import qualified Data.Maybe as DM
 data Path = Path
   {
     _directory :: String
-  } deriving (Show,Read)
+  } deriving (Show,Read,Eq)
+
+data MasterIp = MasterIp
+  {
+    _ip :: String
+  } deriving (Show,Read,Eq)
 
 type Paths = [Path]
 
 data UserConfig = UserConfig
   {
-    _paths :: Paths
-  } deriving (Show,Read)
+      _masterip :: MasterIp
+    , _paths :: Paths
+  } deriving (Show,Read,Eq)
 
 
-
-
+data Parsed =   ParsedPath Path
+              | ParsedMasterIp MasterIp
+              deriving (Show,Read,Eq)
 
 
 parse :: [String] -> UserConfig
-parse [] = UserConfig []
-parse ss = UserConfig $ DM.catMaybes $ map parseLine ss
+parse [] = UserConfig (MasterIp "") $ [] 
+parse ss = mkUserConfig $ DM.catMaybes $ map parseLine ss
 
-parseLine :: String -> Maybe Path
+mkUserConfig :: [Parsed] -> UserConfig
+mkUserConfig prs = UserConfig ip ps
+  where
+    (ParsedMasterIp ip) = head $ filter (\case ParsedMasterIp _ -> True; _ -> False) prs 
+    ps = foldr reducer [] prs
+
+    reducer :: Parsed -> Paths -> Paths
+    reducer (ParsedPath p) l = p : l
+    reducer _ l = l
+
+    
+parseLine :: String -> Maybe Parsed
 parseLine s = maybeUserConfig
   where
     maybeUserConfig = parseWords $ words s
 
-    parseWords :: [String] -> Maybe Path
-    parseWords ("path" : "=" : args) = Just $ Path $ DL.intercalate " " args
+    parseWords :: [String] -> Maybe Parsed
+    parseWords ("path" : "=" : args) = Just $ ParsedPath $ Path $ DL.intercalate " " args
+    parseWords ("masterip" : "=" : args) = Just $ ParsedMasterIp $ MasterIp $ DL.intercalate " " args
     parseWords args = error $ "could not find path = in " ++ (DL.intercalate " " args)
     
--- tzubox/test/LibSpec.hs
+++ tzubox/test/LibSpec.hs
@@ -3,15 +3,21 @@ module LibSpec where
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
-import Lib (ourAdd)
+
+import qualified UserConfig as UC
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec =
-  describe "Lib" $ do
-    it "works" $ do
-      True `shouldBe` True
-    prop "ourAdd is commutative" $ \x y ->
-      ourAdd x y `shouldBe` ourAdd y x
+  describe "Parsing config files" $ do
+    it "parsing paths and masterip should work" $ do
+      let line1 = "path = a"
+      let line2 = "path = ab"
+      let line3 = "masterip = c"
+      UC.parseLine line1 `shouldBe` (Just $ UC.ParsedPath $ UC.Path "a")
+      UC.parseLine line2 `shouldBe` (Just $ UC.ParsedPath $ UC.Path "ab")
+      UC.parseLine line3 `shouldBe` (Just $ UC.ParsedMasterIp $ UC.MasterIp "c")
+--    prop "ourAdd is commutative" $ \x y ->
+--      ourAdd x y `shouldBe` ourAdd y x
